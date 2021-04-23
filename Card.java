fd30601584a7f86494d96cd6bee62e74b015de3c
Ana Beatriz Neri <ananeri32@hotmail.com>
2021-04-22 22:20:07 -0300

    Card class for value

---
+++ Card.java
@@ -0,0 +1,33 @@
+public class Card {
+    public enum Suit { SPADE, HEART, DIAMOND, CLUB }
+    public enum Rank { ACE, DEUCE, THREE, FOUR, FIVE, SIX, SEVEN,
+                       EIGHT, NINE, TEN, JACK, QUEEN, KING }
+
+    private final Suit suit;
+    private final Rank rank;
+
+    @Override public String toString() {
+        return rank + " of " + suit + "S";
+    }
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+
+    }
+    private static final List<Card> NEW_DECK = newDeck();
+
+    // Iterative Cartesian product computation
+    private static List<Card> newDeck() {
+        List<Card> result = new ArrayList<>();
+        for (Suit suit : Suit.values())
+            for (Rank rank : Rank.values())
+                result.add(new Card(suit, rank));
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(NEW_DECK);
+    }
+}
